69ebeb6456ff395c9de826beb3c95fb5ae27e5e7
jesusalbarca <100383415@alumnos.uc3m.es>
2025-03-16 02:34:09 +0100

    fist

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+export default {
+	title: "Biohacking y Bienestar Personal",
+	url: "https://example.com/",
+	language: "es",
+	description: "Descubre cómo mejorar tu salud y rendimiento con biohacking, probióticos, tecnología y hábitos de vida saludables. Análisis de productos, gadgets y tendencias.",
+	author: {
+		name: "JAG",
+		email: "",
+		url: "https://example.com/about-me/"
+	}
+}
